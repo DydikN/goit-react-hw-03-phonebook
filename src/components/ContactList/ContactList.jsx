d9b2452729330226f7a70492e41f5abca47b84d5
DydikN <PingvinPain@gmail.com>
2023-01-29 10:03:17 +0200

    .

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -32,6 +32,6 @@ ContactList.propTypes = {
       id: PropTypes.string.isRequired,
       name: PropTypes.string.isRequired,
       number: PropTypes.string.isRequired,
-    })
+    }).isRequired
   ),
 };
